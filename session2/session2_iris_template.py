03e7c838379183ab8cb7d3bfc54657278e343c47
Akhmal01 <AKHMALBINK_BS23@iluv.ums.edu.my>
2026-04-21 06:46:49 +0000

    session2(done)

--- session2/session2_iris_template.py
+++ session2/session2_iris_template.py
@@ -10,20 +10,20 @@ print("ID:", id)
 # Task 3: Define more variables for one Iris flower
 sepal_length = 5.1
 sepal_width = 3.5  # Uncomment me by click ctrl + / (Windows)
-# petal_length =    # Add appropriate value for petal_length
-petal_width = 100000
-species = "please_change_me"
+petal_length = 1.4  # Add appropriate value for petal_length
+petal_width = 0.2
+species = "setosa"
 
 # Task 3a: Uncomment the print statements below to see the values of the variables you defined.
-# print("Sepal Length:", sepal_length)
-# print("Sepal Width:", sepal_width)
-# print("Petal Length:", petal_length)
-# print("Petal Width:", petal_width)
-# print("Species:", species)
+print("Sepal Length:", sepal_length)
+print("Sepal Width:", sepal_width)
+print("Petal Length:", petal_length)
+print("Petal Width:", petal_width)
+print("Species:", species)
 
 # Task 4: Compute petal area
-# petal_area = ???? * ????
-# print("\nPetal Area:", petal_area)  Uncomment the area
+petal_area = petal_length * petal_width
+print("\nPetal Area:", petal_area)  # Uncomment the area
 
 threshold = 2.0
 feature_name = "petal_length"
@@ -32,4 +32,17 @@ negative_label = "not_setosa"
 label_key = "species"
 
 # Task 5: Comparing with threshold
-# is_short_petal = ???? < threshold
+is_short_petal = petal_length < threshold
+
+sepal_length_2 = 5.1
+sepal_width_2 = 3.5
+petal_length_2 = "1.4"
+petal_width_2 = 0.2
+species_2 = "setosa"
+
+print("petal_length_text:", petal_length_2, "| type:", type(petal_length_2))
+print("threshold_text:", "2.0", "| type:", type("2.0"))
+print("threshold_number:", threshold, "| type:", type(threshold))
+
+petal_area_2 = float(petal_length_2) * petal_width_2
+print("Petal Area 2:", petal_area_2)
